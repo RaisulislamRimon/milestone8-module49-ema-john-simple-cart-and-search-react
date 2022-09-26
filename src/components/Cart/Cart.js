ac2f4046fc82f634cfa18113e3804a62e156b7b3
RaisulislamRimon <rimonppip0@gmail.com>
2022-09-26 14:53:45 +0600

    49-3 Calculate shopping cart price, shipping and tax with toFixed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,14 +3,23 @@ import "./Cart.css";
 
 const Cart = (props) => {
   // const Cart = ({ cart }) => {
-  const cart = props.cart;
+  const { cart } = props;
+  // console.log(cart);
+  // const cart = props.cart;
+  let total = 0;
+  let shipping = 0;
+  for (const product of cart) {
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+  const tax = (total * 0.1).toFixed(2);
   return (
     <div className="cart">
       <h3>Order Summary</h3>
       <p>Selected Items: {cart.length}</p>
-      <p>Total Price: $</p>
-      <p>Total Shipping: $</p>
-      <p>Tax: $</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping: ${shipping}</p>
+      <p>Tax: ${tax}</p>
       <p>Grand Total: $</p>
     </div>
   );
