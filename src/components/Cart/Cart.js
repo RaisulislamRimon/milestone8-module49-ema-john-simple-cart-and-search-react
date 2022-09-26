c20fd908dbe51f7898d098bd9ce27e72a369dffb
RaisulislamRimon <rimonppip0@gmail.com>
2022-09-26 14:32:55 +0600

    cart set sticky position

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Cart.css";
 
 const Cart = (props) => {
   const cart = props.cart;
   return (
-    <div>
+    <div className="cart">
       <h1>this is cart component</h1>
       <h3>This is cart</h3>
       <p>Selected Items: {cart.length}</p>
