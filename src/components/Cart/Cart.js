e930f3c55f4b73405872006462de980adf66c7d9
RaisulislamRimon <rimonppip0@gmail.com>
2022-09-26 14:38:18 +0600

    cart update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = (props) => {
+  // const Cart = ({ cart }) => {
   const cart = props.cart;
   return (
     <div className="cart">
-      <h1>this is cart component</h1>
-      <h3>This is cart</h3>
+      <h3>Order Summary</h3>
       <p>Selected Items: {cart.length}</p>
+      <p>Total Price: $</p>
+      <p>Total Shipping: $</p>
+      <p>Tax: $</p>
+      <p>Grand Total: $</p>
     </div>
   );
 };
