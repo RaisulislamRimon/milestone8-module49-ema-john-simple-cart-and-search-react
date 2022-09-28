8f27a8d5f59cbe638717b1013f638f71c583abf3
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-09-28 18:08:15 +0600

    49-7 (super advanced) Handle quantity from storage to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,26 +9,19 @@ const Cart = (props) => {
   // const cart = props.cart;
   let total = 0;
   let shipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    total = total + product.price;
+    quantity = quantity + product.quantity;
+    total = total + product.price * product.quantity;
     shipping = shipping + product.shipping;
   }
   const tax = (total * 0.1).toFixed(2);
   const grandTotal = total + shipping + Number(tax);
 
-  // const storedCart = getStoredCart();
-  // if (storedCart) {
-  //   console.log(storedCart);
-  // }
-
-  // for (const key in storedCart) {
-  //   console.log(key);
-  // }
-
   return (
     <div className="cart">
       <h3>Order Summary</h3>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${total}</p>
       <p>Total Shipping: ${shipping}</p>
       <p>Tax: ${tax}</p>
